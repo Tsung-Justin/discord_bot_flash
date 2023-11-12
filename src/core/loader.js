922db6211e2006838c8bf05128b4841aecb341f9
Justin Tsung <king90131@gmail.com>
2023-11-13 00:09:29 +0800

    feat: Create loadCommand feature

---
+++ src/core/loader.js
@@ -0,0 +1,45 @@
+import { REST, Routes, Collection } from 'discord.js'
+import fg from 'fast-glob'
+import { useAppStore } from "@/store/app"
+
+const updateSlashCommands = async (commands) => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`)
+
+        const rest = new REST({ version: 10 }).setToken(process.env.TOKEN)
+        const result = await rest.put(
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID
+            ),
+            {
+                body: commands,
+            }
+        )
+
+        console.log(`Successfully reload ${result.length} application (/) commands.`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const loadCommands = async () => {
+    try {
+        const commands = []
+        const appStore = useAppStore()
+        const executes = new Collection()
+        const files = await fg('./src/commands/**/index.js')
+
+        for (const file of files) {
+            const cmd = await import(file)
+            commands.push(cmd.command)
+            executes.set(cmd.command.name, cmd.execute)
+        }
+
+        await updateSlashCommands(commands)
+        appStore.commandsExectionMap = executes
+
+    } catch (error) {
+        console.log(error)
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { loadCommands } from "@/core/loader"
 import dotenv from 'dotenv'
 import vueInit from '@/core/vue'
+import { useAppStore } from '@/store/app'
 
 dotenv.config()
 vueInit()
+loadCommands()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const appStore = new useAppStore()
+appStore.Client = client
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`)
