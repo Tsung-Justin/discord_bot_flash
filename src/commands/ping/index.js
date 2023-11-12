a5985bdd87cdecfcf34a479f096ed0c26fa98984
Justin Tsung <king90131@gmail.com>
2023-11-12 23:42:20 +0800

    feat: Create ping command

---
+++ src/commands/ping/index.js
@@ -0,0 +1,9 @@
+import { SlashCommandBuilder } from 'discord.js'
+
+export const command = new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with pong')
+
+export const execute = async (ctx) => {
+    await ctx.reply('pong!')
+}
